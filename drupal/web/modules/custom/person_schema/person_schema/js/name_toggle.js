25b2f866789b32ebd15ad53a9b1465c661e6b067
Jami <jamidwyer@yahoo.com>
2026-01-23 18:33:03 -0800

    Handle unknown name

--- drupal/web/modules/custom/person_schema/person_schema/js/name_toggle.js
+++ drupal/web/modules/custom/person_schema/person_schema/js/name_toggle.js
@@ -1,19 +1,19 @@
 (function ($, Drupal) {
-  'use strict';
+  "use strict";
 
   Drupal.behaviors.nameToggle = {
     attach: function (context, settings) {
-      var $checkbox = $('#edit-field-name-known-value', context);
-      var $givenName = $('#edit-field-given-name-0-value', context);
-      var $familyName = $('#edit-field-family-name-0-value', context);
+      var $checkbox = $("#edit-field-name-known-value", context);
+      var $givenName = $("#edit-field-given-name-0-value", context);
+      var $familyName = $("#edit-field-family-name-0-value", context);
 
       function toggleNames() {
-        if ($checkbox.is(':checked')) {
-          $givenName.closest('.form-item').show();
-          $familyName.closest('.form-item').show();
+        if ($checkbox.is(":checked")) {
+          $givenName.closest(".form-item").show();
+          $familyName.closest(".form-item").show();
         } else {
-          $givenName.closest('.form-item').hide();
-          $familyName.closest('.form-item').hide();
+          $givenName.closest(".form-item").hide();
+          $familyName.closest(".form-item").hide();
         }
       }
 
@@ -21,8 +21,7 @@
       toggleNames();
 
       // On change.
-      $checkbox.on('change', toggleNames);
-    }
+      $checkbox.on("change", toggleNames);
+    },
   };
-
 })(jQuery, Drupal);
